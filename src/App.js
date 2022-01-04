5630cf36de69a015a0c05a0cd68baa70fedb4e69
BernadetteCh <b.christiner93@gmail.com>
2022-01-04 11:25:24 +0100

    imported new component Dictionary and image, created footer and header,

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import icon from "./books.jpg";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hallo from React</h1>
+      <div className="container">
+        <header><img src={icon} alt="icon of books" className="icon"></img>Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer><a href="https://www.vecteezy.com/free-vector/literature">Literature Vectors by Vecteezy</a></footer>
+      </div>
     </div>
   );
 }
