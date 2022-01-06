d5e683ca1b64599cdb3a2746d2c0252f6226be55
BernadetteCh <b.christiner93@gmail.com>
2022-01-06 17:42:19 +0100

    added links in footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,42 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header><img src={icon} alt="icon of books" className="icon"></img>Dictionary</header>
+        <header>
+          <img src={icon} alt="icon of books" className="icon"></img>Dictionary
+        </header>
         <main>
           <Dictionary />
         </main>
-        <footer><a href="https://www.vecteezy.com/free-vector/literature">Literature Vectors by Vecteezy</a></footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://lucid-williams-1b13b8.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Bernadette
+          </a>
+          , and is open-sourced on{" "}
+          <a
+            href="https://github.com/BernadetteCh/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+            Netflify
+          </a>{" "}
+          <br />{" "}
+          <a
+            href="https://www.vecteezy.com/free-vector/literature"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Literature Vectors by Vecteezy
+          </a>
+        </footer>
       </div>
     </div>
   );
