f0f68c97dbfd25c51d32c536c9d148b5157f1a3f
BernadetteCh <b.christiner93@gmail.com>
2022-01-06 17:45:14 +0100

    changed h3 into a h2 and removed <br />

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,20 +1,20 @@
 import React from "react";
+import "./Meanings.css";
 
 export default function Meanings(props){
     console.log(props.meanings);//i loop durch alle meanings weil a Wort mehr als a meaning und mehr als ein definition haben kann laut der API. in der console.log sieht ma dass es a object of arrey is
     return (
       <div className="Meaning">
-        <h3>{props.meanings.partOfSpeech}</h3>
-        {props.meanings.definitions.map(function(definition,index){
-            return (
-              <div key={index}>
-                <p>
-                  {definition.definition}
-                  <br />
-                  <em>{definition.example}</em>
-                </p>
-              </div>
-            );
+        <div className="partofspeech">
+          <h2>{props.meanings.partOfSpeech}</h2>
+        </div>
+        {props.meanings.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>{definition.definition}</p>
+              <em>{definition.example}</em>
+            </div>
+          );
         })}
       </div>
     );
